e50ac6bb56b88ee6586b1ceb67a3401bfac77905
Eduardo Brito <eduardo370@outlook.pt>
2024-02-23 10:50:24 +0100

    ajust content

--- src/utils/services.tsx
+++ src/utils/services.tsx
@@ -19,7 +19,7 @@ export const services = [
         content: [
             {
                 title: "Gestão Financeira Estratégica",
-                section: "Nossa equipe altamente qualificada coleta e analisa dados financeiros relevantes, incluindo custos, receitas, despesas e investimentos, para fornecer uma visão abrangente da situação financeira de um projeto ou investimento. Utilizamos técnicas e modelos financeiros avançados para prever o desempenho financeiro futuro, considerando uma variedade de cenários e fatores externos, garantindo uma análise completa e precisa.",
+                section: "Analisamos o potencial do seu negócio ou ideia, prestando serviços de aconselhamento estratégico que representam uma importante mais valia na alavancagem de um negócio.",
                 sectionB: "Avaliamos a lucratividade e a sustentabilidade de um projeto ou investimento, utilizando métricas como ROI, payback period, TIR e VPL, para determinar sua viabilidade financeira e potencial de retorno.",
                 sectionC: "Analisamos os riscos associados ao projeto, incluindo fatores econômicos, concorrência e regulamentações governamentais, e desenvolvemos estratégias para mitigar esses riscos e garantir o sucesso financeiro do projeto. Com base em nossa análise detalhada, fornecemos recomendações estratégicas claras e acionáveis para orientar as decisões empresariais, maximizando o valor para os stakeholders e impulsionando o crescimento sustentável.",
                 image: BackgroundService1,
@@ -34,7 +34,7 @@ export const services = [
         content: [
             {
                 title: "Análise de Mercado Detalhada",
-                section: "Nossa equipe especializada realiza uma análise abrangente do mercado-alvo da sua empresa, identificando tendências, demandas dos consumidores, comportamento da concorrência e oportunidades de mercado para informar estratégias de marketing eficazes. Desenvolvemos e conduzimos pesquisas de mercado customizadas para obter insights valiosos sobre as necessidades, preferências e percepções dos clientes, permitindo que você tome decisões estratégicas embasadas em dados sólidos.",
+                section: "Por forma a garantir que atingirá o seu público-alvo da forma mais rentável e eficiente, é essencial o investimento num forte e estruturado plano de comunicação e de marketing, bem como um apurado estudo de mercado.",
                 sectionB: "Com base na análise de mercado e nos insights obtidos, colaboramos com você para desenvolver planos de marketing personalizados e estratégias de posicionamento de marca que sejam eficazes, direcionadas e alinhadas com seus objetivos de negócios. Após o desenvolvimento do plano de marketing, ajudamos a implementar as estratégias definidas e monitoramos continuamente o desempenho, ajustando as táticas conforme necessário para garantir resultados consistentes e maximizar o retorno sobre o investimento.",
                 sectionC: "Realizamos análises regulares dos resultados do plano de marketing, utilizando métricas-chave de desempenho (KPIs) para avaliar o sucesso da estratégia e identificar áreas de melhoria e oportunidades de otimização.",
                 image: BackgroundService2,
@@ -49,7 +49,7 @@ export const services = [
         content: [
             {
                 title: "Avaliação e Diagnóstico Agronômico",
-                section: "Nossa equipe de especialistas realiza uma avaliação detalhada das condições do solo, clima, culturas e práticas agrícolas em sua propriedade, fornecendo um diagnóstico preciso e personalizado para maximizar a produtividade e a sustentabilidade.",
+                section: "Para garantir que o produtor agricola tenha sucesso e alta produtividade prestamos apoio técnico agricola na produção de leguminosas, frutas e hortaliças.",
                 sectionB: "Com base na avaliação agronômica, oferecemos recomendações específicas para cultivo e manejo de culturas, incluindo seleção de culturas adequadas, práticas de plantio, manejo de pragas e doenças, fertilização e irrigação, visando otimizar o rendimento e a qualidade dos produtos agrícolas.",
                 sectionC: "Aconselhamos sobre práticas de conservação de recursos naturais, como manejo sustentável do solo, conservação da água, rotação de culturas e uso eficiente de insumos agrícolas, promovendo a saúde do solo, a biodiversidade e a resiliência do ecossistema agrícola.",
                 image: BackgroundService3,
@@ -64,7 +64,7 @@ export const services = [
         content: [
             {
                 title: "Teaser  and  Due Diligence",
-                section: "Nossa equipe conduz pesquisas e análises detalhadas sobre o cenário de negócios em Angola, incluindo tendências de mercado, comportamento do consumidor, ambiente regulatório e concorrência, identificando oportunidades de negócios lucrativas em diversos setores. ",
+                section: "Para investidores extrangeiros com pretensão de fazer negócios em Angola , oferecemos um serviço especializado com produção de informações sobre o sector de interesse, o tipo de negócio , potenciais parceiros em formatos de  Teaser e due diligence.",
                 sectionB: "Com base em nossa experiência local e conhecimento da indústria, oferecemos serviços de consultoria de investimentos personalizados para orientar investidores no processo de avaliação e aproveitamento de oportunidades de negócios promissoras em Angola, garantindo tomadas de decisão informadas e mitigação de riscos.",
                 sectionC: "Facilitamos parcerias estratégicas e oportunidades de networking conectando investidores a partes interessadas-chave, players da indústria, agências governamentais e empresas locais em Angola, promovendo colaborações que impulsionam o crescimento e a expansão dos negócios. Auxiliamos clientes no desenvolvimento de estratégias eficazes de entrada no mercado adaptadas às características únicas do mercado angolano, incluindo posicionamento de mercado, canais de distribuição, campanhas de marketing e conformidade regulatória, para maximizar suas chances de sucesso.",
                 image: BackgroundService4,
